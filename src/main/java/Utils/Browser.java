4653459f01679e6f3f69512884fb3d2c3428044c
Nitesh Choudhary <niteshc69@gmail.com>
2025-08-08 16:23:56 +0530

    normal changes

--- src/main/java/Utils/Browser.java
+++ src/main/java/Utils/Browser.java
@@ -4,31 +4,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
+
 public class Browser extends File_Reader {
 	public WebDriver driver;
-	
+
 	@BeforeTest
 	public void OpenBrowser() {
-		
+
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications", "--disable-popup-blocking");
 		options.addArguments("--start-maximized");
-		options.addArguments("--incognito");  
+		options.addArguments("--incognito");
 		driver = new ChromeDriver(options);
-			driver.get(websitedetails.getProperty("URL"));
+		driver.get(websitedetails.getProperty("URL"));
 	}
-	
-	
-	
-	
-	//@AfterTest
+
+	// @AfterTest
 	public void CloseBrowser() {
-		
+
 		String Title = driver.getTitle();
-		System.out.println("page title :"+Title);
-		if(Title.contains("Swag Labs")) {
-		driver.quit();
-		
+		System.out.println("page title :" + Title);
+		if (Title.contains("Swag Labs")) {
+			driver.quit();
+
 		}
 	}
 
